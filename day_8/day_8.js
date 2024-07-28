183abc7c2d0342553e6f97b54ca7130d7ecb9f79
addbad618 <ananddhardwivedi05@gmail.com>
2024-07-28 09:04:04 +0530

    ES6+

--- day_8/day_8.js
+++ day_8/day_8.js
@@ -17,8 +17,9 @@ console.log(multiLineString);
 // • Task 3: Use array destructuring to extract the first and second elements from an array of numbers and log them to the console.
 
 const numbers = [1, 2, 3, 4, 5];
-const [first, second] = numbers;
-console.log(first, second);
+//one by one the element of the array is retrieved here
+const [first, second, third, fourthEle] = numbers;
+console.log(first, second, third, fourthEle);
 
 // . Task 4: Use object destructuring to extract the title and author from a book object and log them to the console.
 
