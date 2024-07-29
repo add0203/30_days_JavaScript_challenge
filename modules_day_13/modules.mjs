948d356ddf8a30c3dc5dd704dd5a241306ae4714
addbad618 <ananddhardwivedi05@gmail.com>
2024-07-29 08:38:35 +0530

    modules-webpack

--- modules_day_13/modules.mjs
+++ modules_day_13/modules.mjs
@@ -91,3 +91,4 @@ axios
 
 // Activity 5: Module Bundling (Optional)
 // • Task 8: Use a module bundler like Webpack or Parcel to bundle multiple JavaScript files into a single file. Write a script to demonstrate the bundling process.
+//infolder name web_pack_project
